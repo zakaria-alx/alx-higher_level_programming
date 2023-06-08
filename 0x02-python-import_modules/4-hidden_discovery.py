f199abd53537919fb812069d4ec13445e006d346
zakaria-alx <125534771+zakaria-alx@users.noreply.github.com>
2023-06-08 23:41:05 +0000

    0x02-python-import_modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
     for name in names:
-        if not name.starswith("__"):
+        if name[:2] != "__":
             print(name)
