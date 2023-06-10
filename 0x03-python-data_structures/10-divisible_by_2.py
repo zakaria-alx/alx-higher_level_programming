651c5b1e4a0a5a53681cb11f3de90be86f818b45
zakaria-alx <125534771+zakaria-alx@users.noreply.github.com>
2023-06-10 14:40:29 +0000

    0x03-python-data_structures

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = []
+    for i in range(len(my_list)):
+        new_list.append(my_list[1] % 2 == 0)
+    return new_list
---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,15 @@
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
+
+void print_python_list_info(PyObject *p)
+{
+	long int size = PyList_Size(p);
+	int pos;
+	PyListObject *obj = (PyListObject *)p;
+
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+	for (pos = 0; pos < size; pos++)
+		printf("Element %i: %s\n", pos, Py_TYPE(obj->ob_item[pos])->tp_name);
+}
