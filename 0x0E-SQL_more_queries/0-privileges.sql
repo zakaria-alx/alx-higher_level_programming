0f358be52bbe050b7087be6000f25e2201aac079
zakaria-alx <125534771+zakaria-alx@users.noreply.github.com>
2023-08-16 14:48:40 +0100

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,12 @@
 -- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+# SHOW GRANTS FOR 'user_0d_1'@'localhost';
+# SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SELECT
+    user,
+    host,
+    MAX_GRANTS as privileges
+FROM
+    mysql.user
+WHERE
+    user IN ('user_0d_1', 'user_0d_2')
+    AND host = 'localhost';
